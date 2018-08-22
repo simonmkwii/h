6e17db880ad3a04454e64f581b32f65111136b72
simonmkwii-dev <40786398+simonmkwii-dev@users.noreply.github.com>
2018-08-23 03:10:04 +1000

    h
    h

---
+++ h/Properties/h.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("h")]
+[assembly: AssemblyDescription("h")]
+[assembly: AssemblyConfiguration("h")]
+[assembly: AssemblyCompany("h")]
+[assembly: AssemblyProduct("h")]
+[assembly: AssemblyCopyright("h")]
+[assembly: AssemblyTrademark("h")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b4fa62c5-a79e-4a65-b603-0f051a872c2c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ h/h.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Net;
+
+namespace h
+{
+    class h
+    {
+        static void H(string h_url, string h_content, string h_token)
+        {
+            try
+            {
+                var h = WebRequest.Create(h_url);
+
+                h.Method = "POST";
+                h.ContentType = "application/json";
+                h.Headers.Add("authorization", h_token);
+                h.GetRequestStream().Write(Encoding.ASCII.GetBytes(h_content), 0, h_content.Length);
+                h.GetResponse();
+                h.Abort();
+
+                Console.WriteLine("h");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("oof rate limited");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length >= 1)
+            {
+                while (true)
+                {
+                    H
+                        (
+                        "https://discordapp.com/api/v6/channels/322148780080103435/messages",
+                        "{\"content\":\"h\"}",
+                        args[0]
+                        );
+                }
+            }
+            else
+            {
+                Console.WriteLine("h: h.exe <token>");
+            }
+        }
+    }
+}
